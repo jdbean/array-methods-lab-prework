c211039c30dfafa1fcaaa6bc906b24c557bba97e
J.D. Bean <jonathandbean@gmail.com>
2018-02-15 19:51:25 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+  array.includes?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.return
 end
 
 def using_first(array)
-
+  array.return
 end
 
 def using_last(array)
-
+  array.first
 end
 
 def using_size(array)
-
+  array.size
 end
